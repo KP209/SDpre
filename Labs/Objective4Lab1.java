621540c1da6603a9a851376716100a67d526d357
Kevin <poklay.kw@gmail.com>
2023-04-25 10:38:30 -0700

    revised objective4lab1

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -7,16 +7,12 @@ public class Objective4Lab1 {
 
 		System.out.println("What is your first name?");
 		String fname = keyboard.nextLine();
-
 		System.out.println("What is your last name?");
 		String lname = keyboard.nextLine();
-
 		System.out.println("What is your favorite animal?");
 		String favoriteAnimal = keyboard.nextLine();
-
 		System.out.println("What is your favorite food?");
 		String favoriteFood = keyboard.nextLine();
-
 		System.out.println("What is your favorite song?");
 		String favoriteSong = keyboard.nextLine();
 
@@ -24,5 +20,7 @@ public class Objective4Lab1 {
 		System.out.println("My favorite animal is the " + favoriteAnimal);
 		System.out.println("My favorite food is " + favoriteFood);
 		System.out.println("My favorite song is " + favoriteSong);
+
+		keyboard.close();
 	}
-}
+}	
