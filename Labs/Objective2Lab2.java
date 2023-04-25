5a97d64eed7a96b23f3100fa5de85c8698f09385
Kevin <poklay.kw@gmail.com>
2023-04-24 17:32:18 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Kevin Poklay";
+		int age = 33;
+		String state = "California";
+
+		System.out.print("Name: ");
+		System.out.print(name);
+		System.out.print(" Age: ");
+		System.out.print(age );
+		System.out.print(" State: ");
+		System.out.println(state );
+	}
+}
