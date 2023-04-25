4eda609c39570b13a5650a0cfab0437a923fd9bf
Kevin <poklay.kw@gmail.com>
2023-04-24 18:01:08 -0700

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int birthYear = 1989;
+		String birthMonth = "May";
+		boolean weekendOrNot = false;
+
+		System.out.println(birthYear);
+		System.out.println(birthMonth);
+		System.out.println(weekendOrNot);
+	}
+}
